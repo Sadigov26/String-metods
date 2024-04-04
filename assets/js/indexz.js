1bafa57816d9eabd2539a442c5b39d5e627912b0
Kamil Sadigov <101421625+Sadigov26@users.noreply.github.com>
2024-04-04 08:14:21 +0400

    Add files via upload

---
+++ assets/js/indexz.js
@@ -0,0 +1,32 @@
+// let work = "    Mən Code Academydə Programing tədrisi alıram    ";
+
+// let trimmedWork = work.trim();
+// let letters = trimmedWork.split(" ");
+// console.log(trimmedWork.toUpperCase())
+
+
+// var str = "mazda,opel,toyota"
+// var yeniStr = str.replace("mazda", "honda");
+
+// console.log(yeniStr);
+
+// let text = "ELI VELIYEV "
+// let text1 = "SUMQAYITDA YASAYIR"
+// let result = text.concat(text1);
+// console.log(result);
+
+
+// let sentence = "    Mən Code Academydə Programing tədrisi alıram    ";
+// let trimmedWork = sentence.trim();
+// const index = trimmedWork.indexOf("C");
+// console.log(index);
+
+const arr = [2,4,5,9,1]
+// const tersine = arr.reverse();
+const length = arr.length;
+for (let i = 0; i < array.length; i++) {
+  const element = array[i];
+  
+}
+console.log(arr);
+
